eb3911a8f11e7a2e7c5941d4d4d622d86ea8fe2e
Mikegunawan <105189121+Mikegunawan@users.noreply.github.com>
2022-05-09 12:03:39 +0700

    file 1

---
+++ PH.js
@@ -0,0 +1,67 @@
+let Gadget = [] ;
+
+let HP = {
+foto: "http://p-id.ipricegroup.com/uploaded_64513a699f84dfd2e2b5273c516811a4.jpg",
+harga: 22000000,
+kode: "BRG001",
+nama: "iPhone 13 Pro Max",
+rating: "5.0",
+stock: 10,
+}
+
+Gadget.push(HP);
+
+HP={
+foto: "https://images.samsung.com/pk/smartphones/galaxy-z-flip3-5g/buy/zflip3_carousel_foldunfoldcombo_lavender_mo.jpg",
+harga: 50000000,
+kode: "BRG002",
+nama: "Samsung Galaxy Flip3 5G",
+rating: "5.0",
+stock: 20,
+}
+
+Gadget.push(HP);
+
+HP={
+foto: "https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-5g-1-500x500.jpg",
+harga: 22000000,
+kode: "BRG001",
+nama: "Oppo Reno 9",
+rating: "5.0",
+stock: 100,
+}
+
+Gadget.push(HP);
+
+HP={
+foto: "https://akcdn.detik.net.id/visual/2022/01/27/xiaomi-redmi-note-11-6_169.png?w=650",
+harga: 50000000,
+kode: "BRG004",
+nama: "Xiaomi Redmi Note 11",
+rating: "5.0",
+stock: 1000,
+}
+
+Gadget.push(HP);
+    
+HP={
+foto: "https://d1n6dbtoa2690v.cloudfront.net/article/5d9ad149b92c2e3e758b460b/5d9ad149b92c2e3e758b460b_1595562715.jpg",
+harga: 60000000,
+kode: "BRG005",
+nama: "Vivo",
+rating: "5.0",
+stock: 50,
+}
+
+Gadget.push(HP);
+
+HP= {
+    kode: "S22",
+    nama: "Samsung S22",
+    harga: 11000000,
+    rating: "4.5",
+    stock: 31,
+    foto: "https://images.samsung.com/id/smartphones/galaxy-s22-ultra/buy/S22_Ultra_ProductKV_White_MO.jpg"
+}
+Gadget.push(HP);
+console.log(Gadget);
